8c7e312ca72fd4049050e2ad8a41658f747a2e09
Alexis AMOI <alexisamoi@Alexiss-MacBook-Pro.local>
2026-01-22 17:49:52 -0500

    update stock

---
+++ contracts/VendingMachine.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract VendingMachine {
+    address public owner;
+    mapping (address => uint) public donutBalances;
+
+    constructor() {
+        owner = msg.sender;
+        donutBalances[address(this)] = 700; // Initial stock
+    }
+
+    function purchase(uint amount) public payable {
+        require(msg.value >= amount * 0.1 ether, "Not enough Ether sent.");
+        require(donutBalances[address(this)] >= amount, "Not enough donuts in stock.");
+        donutBalances[address(this)] -= amount;
+        donutBalances[msg.sender] += amount;
+    }
+}
